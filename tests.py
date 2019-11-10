0d0388a7d833f16a55d055a403015383207fa2d6
Vitaly-R <vitaly.rajev@gmail.com>
2019-11-10 18:01:00 +0200

    runs on laptop

--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import warnings
 warnings.filterwarnings('ignore', category=FutureWarning)
 from operations import *
+np.random.seed(0)
 
 
 def main():
     x = np.random.randint(-100, 100, 10)
-    print(relu_numpy(x), re)
+    print(relu_numpy(x))
 
 
 if __name__ == '__main__':
