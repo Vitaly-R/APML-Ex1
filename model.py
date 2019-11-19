be0a431839af64c466c3a701cf7d62b212eea49e
Vitaly Rajev <vitaly.rajev@gmail.com>
2019-11-19 19:05:45 +0200

    added regularization option, also added relu activation to convolutions

--- model.py
+++ model.py
@@ -1,18 +1,19 @@
 import tensorflow as tf
 
 
-def mlp(x: tf.Tensor, nlabels):
+def mlp(x: tf.Tensor, nlabels, regularizer=None):
     """
     multi layer perceptrone: x -> linear > relu > linear.
     :param x: symbolic tensor with shape (batch, 28, 28)
     :param nlabels: the dimension of the output.
+    :param regularizer: optional parameter - a regularizer for the variables
     :return: a symbolic tensor, the result of the mlp, with shape (batch, nlabels). the model return logits (before softmax).
     """
     # ---------- defining variables ----------
-    w1 = tf.get_variable('weights1', shape=[100, x.shape[1] * x.shape[2]], dtype=tf.float32, trainable=True)
-    b1 = tf.get_variable('bias1', shape=[100, 1], dtype=tf.float32, trainable=True)
-    w2 = tf.get_variable('weights2', shape=[nlabels, 100], dtype=tf.float32, trainable=True)
-    b2 = tf.get_variable('bias2', shape=[nlabels, 1], dtype=tf.float32, trainable=True)
+    w1 = tf.get_variable('weights1', shape=[100, x.shape[1] * x.shape[2]], dtype=tf.float32, trainable=True, regularizer=regularizer)
+    b1 = tf.get_variable('bias1', shape=[100, 1], dtype=tf.float32, trainable=True, regularizer=regularizer)
+    w2 = tf.get_variable('weights2', shape=[nlabels, 100], dtype=tf.float32, trainable=True, regularizer=regularizer)
+    b2 = tf.get_variable('bias2', shape=[nlabels, 1], dtype=tf.float32, trainable=True, regularizer=regularizer)
 
     # --------- reshaping ------------
     x_flattened = tf.reshape(x, (x.shape[0], x.shape[1] * x.shape[2]))
@@ -29,31 +30,34 @@ def mlp(x: tf.Tensor, nlabels):
     return result
 
 
-def conv_net(x: tf.Tensor, nlabels):
+def conv_net(x: tf.Tensor, nlabels, regularizer=None):
     """
     convnet.
     in the convolution use 3x3 filteres with 1x1 strides, 20 filters each time.
     in the  maxpool use 2x2 pooling.
     :param x: symbolic tensor with shape (batch, 28, 28)
     :param nlabels: the dimension of the output.
+    :param regularizer: optional parameter - a regularizer for the variables
     :return: a symbolic tensor, the result of the mlp, with shape (batch, nlabels). the model return logits (before softmax).
     """
     # ---------- reshaping input ----------
     x_re = tf.expand_dims(x, -1)
     # ---------- 1st convolution ----------
-    filter1 = tf.get_variable('filter1', shape=[3, 3, 1, 20], dtype=tf.float32, trainable=True)
+    filter1 = tf.get_variable('filter1', shape=[3, 3, 1, 20], dtype=tf.float32, trainable=True, regularizer=regularizer)
     conv1 = tf.nn.conv2d(x_re, filter1, [1, 1, 1, 1], 'SAME')
+    activation1 = tf.nn.relu(conv1, name='relu1')
     # ---------- 1st max pool ----------
-    max_pool1 = tf.nn.max_pool(conv1, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+    max_pool1 = tf.nn.max_pool(activation1, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
     # ---------- 2nd convolution ----------
-    filter2 = tf.get_variable('filter2', shape=[3, 3, 20, 20], dtype=tf.float32, trainable=True)
+    filter2 = tf.get_variable('filter2', shape=[3, 3, 20, 20], dtype=tf.float32, trainable=True, regularizer=regularizer)
     conv2 = tf.nn.conv2d(max_pool1, filter2, [1, 1, 1, 1], 'SAME')
+    activation2 = tf.nn.relu(conv2, name='relu1')
     # ---------- 2nd max pool ----------
-    max_pool2 = tf.nn.max_pool(conv2, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+    max_pool2 = tf.nn.max_pool(activation2, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
     # ---------- flatten ----------
     flattened = tf.reshape(max_pool2, (max_pool2.shape[0], max_pool2.shape[1] * max_pool2.shape[2] * max_pool2.shape[3]))
     # ---------- linear ----------
-    w = tf.get_variable('weights', shape=[flattened.shape[1], nlabels], dtype=tf.float32, trainable=True)
-    b = tf.get_variable('bias', shape=[1, nlabels], dtype=tf.float32, trainable=True)
+    w = tf.get_variable('weights', shape=[flattened.shape[1], nlabels], dtype=tf.float32, trainable=True, regularizer=regularizer)
+    b = tf.get_variable('bias', shape=[1, nlabels], dtype=tf.float32, trainable=True, regularizer=regularizer)
     result = tf.add(tf.matmul(flattened, w), b)
     return result
--- train.py
+++ train.py
@@ -2,49 +2,85 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from model import mlp, conv_net
+
+
 np.random.seed(0)
 tf.random.set_random_seed(0)
 
 
-def train(model_fn, batch_size, learning_rate=None):
+def normalize(data):
+    res = data - np.mean(data)
+    res = res / np.std(res)
+    return res
+
+
+def batch_dataset(data: np.ndarray, labels: np.ndarray, batch_size):
+    inds = np.arange(data.shape[0])
+    np.random.shuffle(inds)
+    s_data = data[inds]
+    s_labels = labels[inds]
+    split_inds = [(i + 1) * batch_size for i in range(data.shape[0] // batch_size)]
+    data_batches = np.array_split(s_data, split_inds)
+    labels_batches = np.array_split(s_labels, split_inds)
+    if data_batches[-1].shape[0] < batch_size:
+        data_batches.pop(-1)
+        labels_batches.pop(-1)
+    return data_batches, labels_batches
+
+
+def load_datasets(batch_size):
+    ((x_train_np, y_train_np), (x_test_np, y_test_np)) = tf.keras.datasets.fashion_mnist.load_data()
+    n_x_train = normalize(x_train_np)
+    n_x_test = normalize(x_test_np)
+
+    x_train, y_train = batch_dataset(n_x_train, y_train_np, batch_size)
+    x_test, y_test = batch_dataset(n_x_test, y_test_np, batch_size)
+
+    return x_train, y_train, x_test, y_test, len(np.unique(y_train_np))
+
+
+def calculate_accuracy(logits, labels):
+    s_logits = tf.nn.softmax(logits=logits, axis=1).eval()
+    results = np.zeros(s_logits.shape[0]).astype(np.int32)
+    for i in range(results.shape[0]):
+        results[i] = np.argmax(s_logits[i])
+    return round(100 * len(results[results == labels]) / len(results), 5)
+
+
+def plot_graph(to_plot, title):
+    plt.figure()
+    plt.title(title)
+    plt.plot(to_plot)
+
+
+def train(model_fn, batch_size, learning_rate=None, epochs=10, regularize=False):
     """
     load FashionMNIST data.
     create model using model_fn, and train it on FashionMNIST.
     :param model_fn: a function to create the model (should be one of the functions from model.py)
     :param batch_size: the batch size for the training
     :param learning_rate: optional parameter - option to specify learning rate for the optimizer.
+    :param epochs: optional parameter - number of epochs to train the model.
+    :param regularize: optional parameter - option to train the model with regularization.
     :return:
     """
     # ---------- load data and pre-processing ----------
-    ((x_train_np, y_train_np), (x_test_np, y_test_np)) = tf.keras.datasets.fashion_mnist.load_data()
-
-    x_train_np = x_train_np - np.mean(x_train_np)
-    x_train_np = x_train_np / np.std(x_train_np)
-
-    training_inds = list(range(x_train_np.shape[0]))
-    np.random.shuffle(training_inds)
-    x_train_np = x_train_np[training_inds]
-    y_train_np = y_train_np[training_inds]
-
-    x_test_np = x_test_np - np.mean(x_test_np)
-    x_test_np = x_test_np / np.std(x_test_np)
-
-    test_inds = list(range(x_test_np.shape[0]))
-    np.random.shuffle(test_inds)
-    x_test_np = x_test_np[test_inds]
-    y_test_np = y_test_np[test_inds]
-
-    n_labels = len(np.unique(y_train_np))
+    x_train, y_train, x_test, y_test, n_labels = load_datasets(batch_size)
 
     # ---------- set placeholders ----------
     x_ph = tf.placeholder(tf.float32, shape=[batch_size, 28, 28], name='X_input')
     y_ph = tf.placeholder(tf.int32, shape=[batch_size], name='Y_input')
 
     # ---------- define model ----------
-    logits = model_fn(x_ph, n_labels)
-
-    # ---------- define loss function and optimizer ----------
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_ph, logits=logits, name='loss')
+    regularizer = tf.contrib.layers.l2_regularizer(scale=0.1) if regularize else None
+    model = model_fn(x_ph, n_labels, regularizer=regularizer)
+    loss_func = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_ph, logits=model, name='loss')
+    if regularize:
+        reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        reg_term = tf.contrib.layers.apply_regularization(regularizer, reg_variables)
+        loss = loss_func + reg_term
+    else:
+        loss = loss_func
 
     if learning_rate is None:
         optimizer = tf.train.AdamOptimizer().minimize(loss)
@@ -54,66 +90,70 @@ def train(model_fn, batch_size, learning_rate=None):
     # ---------- initializer ----------
     init = tf.global_variables_initializer()
 
+    # ---------- collecting results ----------
+    training_losses = list()
+    training_accuracies = list()
+    test_losses = list()
+    test_accuracies = list()
+
     with tf.Session() as sess:
         sess.run(init)
+        for epoch in range(1, epochs + 1):
 
-        # ---------- training ----------
-        training_losses = list()
-        training_accuracies = list()
-        for j in range(x_train_np.shape[0] // batch_size):
-            x_training_batch = x_train_np[j * batch_size: (j + 1) * batch_size]
-            y_training_batch = y_train_np[j * batch_size: (j + 1) * batch_size]
-            loss_val, _, training_logits = sess.run([loss, optimizer, logits], feed_dict={x_ph: x_training_batch, y_ph: y_training_batch})
-            e_training_logits = np.exp(training_logits)
-            for v in e_training_logits:
-                v = v / np.sum(v)
-            results = np.zeros(e_training_logits.shape[0]).astype(np.int32)
-            for i in range(results.shape[0]):
-                results[i] = np.argmax(e_training_logits[i])
-            round_loss = np.mean(loss_val)
-            round_accuracy = len(results[results == y_training_batch]) / len(results)
-            training_accuracies.append(round_accuracy)
-            training_losses.append(round_loss)
-            print('training round {}, loss: {}, accuracy: {}'.format(j, round_loss, round_accuracy)) if not j % 10 else None
+            e_training_losses = list()
+            e_training_accuracies = list()
+            e_test_losses = list()
+            e_test_accuracies = list()
 
-        # ---------- testing ----------
-        test_losses = list()
-        test_accuracies = list()
-        one_hot_test_y = np.zeros((y_test_np.shape[0], n_labels))
-        one_hot_test_y[np.arange(y_test_np.shape[0]), y_test_np] = 1
-        for j in range(x_test_np.shape[0] // batch_size):
-            x_test_batch = x_test_np[j * batch_size: (j + 1) * batch_size]
-            y_test_batch = y_test_np[j * batch_size: (j + 1) * batch_size]
-            test_logits, loss_vals = sess.run([logits, loss], feed_dict={x_ph: x_test_batch, y_ph: y_test_batch})
-            e_logits = np.exp(test_logits)
-            for i in range(e_logits.shape[0]):
-                e_logits[i] = e_logits[i] / np.sum(e_logits[i])
-            results = np.zeros(e_logits.shape[0]).astype(np.int32)
-            for i in range(results.shape[0]):
-                results[i] = np.argmax(e_logits[i])
-            test_losses.append(np.mean(loss_vals))
-            test_accuracies.append(len(results[results == y_test_batch]) / len(results))
+            # ---------- training ----------
+            training_counter = 1
+            for data, labels in zip(x_train, y_train):
+                losses, _, logits = sess.run([loss, optimizer, model], feed_dict={x_ph: data, y_ph: labels})
+                round_loss = np.mean(losses)
+                round_accuracy = calculate_accuracy(logits, labels)
+                training_losses.append(round_loss)
+                training_accuracies.append(round_accuracy)
+                e_training_losses.append(round_loss)
+                e_training_accuracies.append(round_accuracy)
+                print('Epoch {}, training round {}, loss: {}, accuracy: {}%'.format(epoch, training_counter, round_loss, round_accuracy)) \
+                    if training_counter == 1 or not training_counter % 100 else None
+                training_counter += 1
+
+            # ---------- testing ----------
+            test_counter = 1
+            for t_data, t_labels in zip(x_test, y_test):
+                logits, losses = sess.run([model, loss], feed_dict={x_ph: t_data, y_ph: t_labels})
+                round_loss = np.mean(losses)
+                round_accuracy = calculate_accuracy(logits, t_labels)
+                test_losses.append(round_loss)
+                test_accuracies.append(round_accuracy)
+                e_test_losses.append(round_loss)
+                e_test_accuracies.append(round_accuracy)
+                print('Epoch {}, test round {}, loss: {}, accuracy: {}%'.format(epoch, test_counter, round_loss, round_accuracy)) if test_counter == 1 or not test_counter % 50 else None
+                test_counter += 1
+
+            # ---------- informing the user ----------
+            template = 'Epoch {} summary - training loss: {}, training accuracy: {}%, test loss: {}, test accuracy: {}%\n'
+            print(template.format(epoch, np.mean(e_training_losses), np.mean(e_training_accuracies), np.mean(e_test_losses), np.mean(e_test_accuracies)))
 
     # ---------- plotting the graphs ----------
-    plt.figure()
-    plt.title('training loss values')
-    plt.plot(training_losses)
-    plt.figure()
-    plt.title('training accuracies')
-    plt.plot(training_accuracies)
-    plt.figure()
-    plt.title('test loss values')
-    plt.plot(test_losses)
-    plt.figure()
-    plt.title('test accuracies')
-    plt.plot(test_accuracies)
-
-    plt.show()
+    func_title = 'MLP\n' if model_fn == mlp else 'ConvNet\n'
+    reg_title = ', with regularization' if regularize else ', without regularization'
+    plot_graph(training_losses, func_title + 'training loss values\nwith {} epochs, and batch size {}'.format(epochs, batch_size) + reg_title)
+    plot_graph(training_accuracies, func_title + 'training accuracies values\nwith {} epochs, and batch size {}'.format(epochs, batch_size) + reg_title)
+    plot_graph(test_losses, func_title + 'test loss values\nwith {} epochs, and batch size {}'.format(epochs, batch_size) + reg_title)
+    plot_graph(test_accuracies, func_title + 'test accuracy values\nwith {} epochs, and batch size {}'.format(epochs, batch_size) + reg_title)
 
 
 def main():
-    # train(mlp, 64)
-    train(conv_net, 64)
+    train(mlp, 100, epochs=10, regularize=True)
+    tf.reset_default_graph()
+    train(mlp, 100, epochs=10)
+    # tf.reset_default_graph()
+    # train(conv_net, 64, epochs=1, regularize=True)
+    # tf.reset_default_graph()
+    # train(conv_net, 64, epochs=1)
+    plt.show()
 
 
 if __name__ == "__main__":
